ccca310007fb78eb336183711d9a1121bcd1ba82
Paco Fina <pfina@pipelinestudios.com>
2022-04-11 05:46:38 -0400

    __init__ changes.

--- __init__.py
+++ __init__.py
@@ -1,6 +1,4 @@
 from .ui   import channelbox
-from .node import Node, NodeList, NodeAttribute, Namespace
-
-reload( channelbox )
+from .node import Node, NodeList, NodeAttribute, Namespace, registerCustomType
 
 Node.getSelectedAttrs = channelbox.get_selected_attrs
